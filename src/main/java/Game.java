e06e75bda4b3781831a11759883ca401b7d58df9
javier mera <javier_mera88@hotmail.com>
2016-08-21 12:12:31 -0500

    Administrator can now specify type of Item in jar, and max amount of items.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,12 @@
+import java.io.Console;
+
 public class Game {
     public static void main(String[] args) {
 
+        Console console = System.console();
+        String itemType = console.readLine("Enter the type of item: ");
+        int maxItemAmount = Integer.parseInt(console.readLine("Enter max amount of items: "));
+
+        console.printf("The maximum amount of %s, is %d", itemType, maxItemAmount);
     }
 }
